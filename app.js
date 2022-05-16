837211b51c5cb29d57a8fd646748251f77ae4aa1
Gioele-M <gioelemontis97@gmail.com>
2022-05-16 16:44:36 +0100

    post and delete endpoints work

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
 	// .get requires the page
-	res.send('Hello Auguste!'); //This is what's put on the page
+	res.status(200).send('Hello Auguste!'); //This is what's put on the page
 });
 
 // Get all jokes
 app.get('/jokes', (req, res) => {
-	res.send(data.objectTypeJokeMany);
+	res.status(200).send(data.objectTypeJokeMany);
 	// --------- remember to define jokes
 });
 
@@ -29,7 +29,7 @@ app.get('/jokes/:id', (req, res) => {
 		if (!joke) {
 			throw new Error('This joke does not exist');
 		} else {
-			res.send(joke);
+			res.status(200).send(joke);
 		}
 	} catch (err) {
 		res.status(404).send({ message: err.message });
@@ -40,17 +40,34 @@ app.get('/jokes/:id', (req, res) => {
 
 
 
-
 // Post new joke
 app.post('/jokes/new', (req, res) => {
-	const newJokeId = jokes.length + 1;
+    // Adjust to get index of last item as if elements are deleted will overlap
+	//const newJokeId = data.objectTypeJokeMany.length + 1
+    
+    const newJokeId = data.objectTypeJokeMany[data.objectTypeJokeMany.length - 1].id + 1
 
-	const newJoke = { ...req.body, id: newJokeId };
-	jokes.push(newJoke);
-	res.status(201).send(newJoke);
-});
+	const newJoke = { ...req.body, id: newJokeId }
+	data.objectTypeJokeMany.push(newJoke);
+	res.status(201).send(`The joke was posted! Joke id: ${newJokeId}`);
+}
+)
+
+
 
 // Delete specific joke
-app.delete('/jokes/:id', (req, res) => {});
+app.delete('/jokes/:id', (req, res) => {
+    // from req body 
+    // either : data.find(?) -> look for array method that matches object
+
+    // Longer solution
+    // req.body.id -> find this id in array -> get index -> remove by index
+
+    let indexToRemove = req.body.id
+    data.objectTypeJokeMany.splice(req.body.id -1, 1)
+    
+    console.log('should have worked')
+    res.status(204).end()
+});
 
 module.exports = app;
--- data.js
+++ data.js
@@ -139,45 +139,24 @@ let objectTypeJokeMany = [
         ]
     },
     {
-        id: 3, // Will be incremental
-        jokeText: 'Based on user input, refer to input in html',
-        jokeEmoji: '#', //url to emoji(?) Give user possibility to insert emoji, blank if null
-        jokeReactions: {
-            emoji1: 0,
-            emoji2: 0,
-            emoji3: 0 // This will be accessed and modified with patch (? can we do += 1 instead of retrieving this)
+        "id": 3, 
+        "jokeText": "Based on user input, refer to input in html",
+        "jokeEmoji": "#",
+        "jokeReactions": {
+            "emoji1": 0,
+            "emoji2": 0,
+            "emoji3": 0 
         },
-        comments: [
-            // Will this point to the endpoints or stores the object themselves?
-            // for the moment it stores an array of comments objects
+        "comments": [
             {
-                commentID: 1, 
-                commentText: 'Based on user input as well!',
-                commentReactions:{
-                    emoji1: 0, 
-                    emoji2: 0,
-                    emoji3: 0 
-                },
-            },
-            {
-                commentID: 2, 
-                commentText: 'Based on user input as well!',
-                commentReactions:{
-                    emoji1: 0, 
-                    emoji2: 0,
-                    emoji3: 0 
-                },
-            },
-            {
-                commentID: 3, 
-                commentText: 'Based on user input as well!',
-                commentReactions:{
-                    emoji1: 0, 
-                    emoji2: 0,
-                    emoji3: 0 
+                "commentID": 1, 
+                "commentText": "Based on user input as well!",
+                "commentReactions":{
+                    "emoji1": 0, 
+                    "emoji2": 0,
+                    "emoji3": 0 
                 },
             }
-    
         ]
     }
 ]
@@ -185,6 +164,26 @@ let objectTypeJokeMany = [
 
 
 
+const sampleRequest = {
+    "jokeText": "This is a sample text",
+    "jokeEmoji": "#",
+    "jokeReactions": {
+        "emoji1": 0,
+        "emoji2": 0,
+        "emoji3": 0 
+    },
+    "comments": [
+        {
+            "commentID": 1, 
+            "commentText": "This is a sample too!",
+            "commentReactions":{
+                "emoji1": 0, 
+                "emoji2": 0,
+                "emoji3": 0 
+            }
+        }
+    ]
+}
 
 
 
@@ -193,5 +192,4 @@ let objectTypeJokeMany = [
 
 
 
-
-module.exports = {objectTypeJoke, objectTypeComment, objectTypeJokeMany}
+module.exports = {objectTypeJoke, objectTypeComment, objectTypeJokeMany, sampleRequest}

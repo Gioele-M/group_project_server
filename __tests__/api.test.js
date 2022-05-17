a151825a8c864b9732d18632b627c8a7b495294c
Rebecca Blixt <rebecca.blixt@hotmail.com>
2022-05-17 11:34:13 +0200

    add tests on endpoint to get all jokes & get joke by id

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -53,4 +53,57 @@ describe('API server', () => {
 		console.log('Gracefully stopping test server');
 		api.close(done);
 	});
+
+	test('it responds to get /jokes with status 200', (done) => {
+		request(api).get('/jokes').expect(200, done);
+	});
+
+	test('retrieves a joke by id', (done) => {
+		request(api)
+			.get('/jokes/2')
+			.expect(
+				{
+					id: 2,
+					jokeText: 'Based on user input, refer to input in html',
+					jokeEmoji: '#',
+					jokeReactions: {
+						emoji1: 0,
+						emoji2: 0,
+						emoji3: 0,
+					},
+					comments: [
+						{
+							commentID: 1,
+							commentText: 'Based on user input as well!',
+							commentReactions: {
+								emoji1: 0,
+								emoji2: 0,
+								emoji3: 0,
+							},
+						},
+						{
+							commentID: 2,
+							commentText: 'Based on user input as well!',
+							commentReactions: {
+								emoji1: 0,
+								emoji2: 0,
+								emoji3: 0,
+							},
+						},
+						{
+							commentID: 3,
+							commentText: 'Based on user input as well!',
+							commentReactions: {
+								emoji1: 0,
+								emoji2: 0,
+								emoji3: 0,
+							},
+						},
+					],
+				},
+				done
+			);
+	});
+
+	test('');
 });

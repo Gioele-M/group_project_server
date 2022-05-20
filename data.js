cc3698d83e468ddd49e70ea7054f6a665dd8aa5c
Gioele-M <gioelemontis97@gmail.com>
2022-05-20 10:48:10 +0100

    add sample jokes

--- data.js
+++ data.js
@@ -1,41 +1,32 @@
 
 let objectTypeJoke ={
     id: 1, // Will be incremental
-    jokeText: 'Based on user input, refer to input in html',
+    jokeText: 'I went to the zoo the other day, there was only one dog in it...',
     jokeEmoji: '#', //url to emoji(?) Give user possibility to insert emoji, blank if null
     jokeReactions: {
-        emoji1: 0,
-        emoji2: 0,
-        emoji3: 0 // This will be accessed and modified with patch (? can we do += 1 instead of retrieving this)
+        emoji1: 20,
+        emoji2: 5,
+        emoji3: 8 // This will be accessed and modified with patch (? can we do += 1 instead of retrieving this)
     },
     comments: [
         // Will this point to the endpoints or stores the object themselves?
         // for the moment it stores an array of comments objects
         {
             commentID: 1, 
-            commentText: 'Based on user input as well!',
+            commentText: 'It was a shitzu!',
             commentReactions:{
-                emoji1: 0, 
-                emoji2: 0,
-                emoji3: 0 
+                emoji1: 25, 
+                emoji2: 10,
+                emoji3: 5 
             },
         },
         {
             commentID: 2, 
-            commentText: 'Based on user input as well!',
+            commentText: 'Ahahahaha how funny boi',
             commentReactions:{
-                emoji1: 0, 
+                emoji1: 30, 
                 emoji2: 0,
-                emoji3: 0 
-            },
-        },
-        {
-            commentID: 3, 
-            commentText: 'Based on user input as well!',
-            commentReactions:{
-                emoji1: 0, 
-                emoji2: 0,
-                emoji3: 0 
+                emoji3: 2 
             },
         }
 
@@ -56,41 +47,32 @@ let objectTypeComment ={
 let objectTypeJokeMany = [
     {
         id: 1, // Will be incremental
-        jokeText: 'Based on user input, refer to input in html',
+        jokeText: 'I went to the zoo the other day, there was only one dog in it...',
         jokeEmoji: '#', //url to emoji(?) Give user possibility to insert emoji, blank if null
         jokeReactions: {
-            emoji1: 0,
-            emoji2: 0,
-            emoji3: 0 // This will be accessed and modified with patch (? can we do += 1 instead of retrieving this)
+            emoji1: 20,
+            emoji2: 5,
+            emoji3: 8 // This will be accessed and modified with patch (? can we do += 1 instead of retrieving this)
         },
         comments: [
             // Will this point to the endpoints or stores the object themselves?
             // for the moment it stores an array of comments objects
             {
                 commentID: 1, 
-                commentText: 'Based on user input as well!',
+                commentText: 'It was a shitzu!',
                 commentReactions:{
-                    emoji1: 0, 
-                    emoji2: 0,
-                    emoji3: 0 
+                    emoji1: 25, 
+                    emoji2: 10,
+                    emoji3: 5 
                 },
             },
             {
                 commentID: 2, 
-                commentText: 'Based on user input as well!',
+                commentText: 'Ahahahaha how funny boi',
                 commentReactions:{
-                    emoji1: 0, 
+                    emoji1: 30, 
                     emoji2: 0,
-                    emoji3: 0 
-                },
-            },
-            {
-                commentID: 3, 
-                commentText: 'Based on user input as well!',
-                commentReactions:{
-                    emoji1: 0, 
-                    emoji2: 0,
-                    emoji3: 0 
+                    emoji3: 2 
                 },
             }
     
@@ -98,30 +80,30 @@ let objectTypeJokeMany = [
     },
     {
         id: 2, // Will be incremental
-        jokeText: 'Based on user input, refer to input in html',
+        jokeText: 'A woman gets on a bus with her baby. The driver says, “Ugh! That’s the ugliest baby I’ve ever seen. The woman stalks off to the rear of the bus and sits down. She turns to the man sitting next to her and says, “The driver just insulted me!”',
         jokeEmoji: '#', //url to emoji(?) Give user possibility to insert emoji, blank if null
         jokeReactions: {
-            emoji1: 0,
-            emoji2: 0,
-            emoji3: 0 // This will be accessed and modified with patch (? can we do += 1 instead of retrieving this)
+            emoji1: 10,
+            emoji2: 12,
+            emoji3: 5 // This will be accessed and modified with patch (? can we do += 1 instead of retrieving this)
         },
         comments: [
             // Will this point to the endpoints or stores the object themselves?
             // for the moment it stores an array of comments objects
             {
                 commentID: 1, 
-                commentText: 'Only two comments!',
+                commentText: 'The man says, “You go and give him a telling off. I’ll hold your monkey for you.”',
                 commentReactions:{
-                    emoji1: 0, 
-                    emoji2: 0,
-                    emoji3: 0 
+                    emoji1: 10, 
+                    emoji2: 5,
+                    emoji3: 5 
                 },
             },
             {
                 commentID: 2, 
-                commentText: 'Based on user input as well!',
+                commentText: 'Dyslexic man walks into a bra!',
                 commentReactions:{
-                    emoji1: 0, 
+                    emoji1: 150, 
                     emoji2: 0,
                     emoji3: 0 
                 },
@@ -131,7 +113,7 @@ let objectTypeJokeMany = [
     },
     {
         "id": 3, 
-        "jokeText": "Based on user input, refer to input in html",
+        "jokeText": "Police arrested two kids yesterday, one was drinking battery acid, the other was eating fireworks.",
         "jokeEmoji": "#",
         "jokeReactions": {
             "emoji1": 0,
@@ -141,7 +123,7 @@ let objectTypeJokeMany = [
         "comments": [
             {
                 "commentID": 1, 
-                "commentText": "Only one comment!!",
+                "commentText": "They charged one – and let the other one off!!",
                 "commentReactions":{
                     "emoji1": 0, 
                     "emoji2": 0,
